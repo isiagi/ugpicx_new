4a2a33b3e5329a80d2c405bb25a0737839d472e1
isiagi <isiagigeofrey0@gmail.com>
2025-06-27 14:27:39 +0300

    modify route

--- app/api/images/[id]/route.ts
+++ app/api/images/[id]/route.ts
@@ -1,43 +1,42 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-// get by userid
 export async function GET(
   request: Request,
-  { params }: { params: Promise<{ id: string }> } // Note the Promise type
+  { params }: { params: { id: string } }
 ) {
-  const { id } = await params; // Await the params object
+  const { id } = params;
 
-  console.log(id);
-
-  //   userId: string
+  console.log("Fetching images for userId:", id);
 
   try {
-    const image = await prisma.image.findMany({
+    const images = await prisma.image.findMany({
       where: { userId: id },
     });
-    if (!image) {
+    if (!images || images.length === 0) {
       return NextResponse.json({ error: "Image not found" }, { status: 404 });
     }
-    return NextResponse.json(image);
+    return NextResponse.json(images);
   } catch (error) {
-    console.log("Failed to fetch image:", error);
+    console.error("Failed to fetch images:", error);
     return NextResponse.json(
-      { error: "Failed to fetch image" },
+      { error: "Failed to fetch images" },
       { status: 500 }
     );
   }
 }
 
-export async function DELETE(request: Request, { params }: { params: any }) {
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
   const { id } = params;
 
   try {
     await prisma.image.delete({ where: { id } });
     return NextResponse.json({ message: "Image deleted successfully" });
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (error: any) {
+  } catch (error) {
+    console.error("Failed to delete image:", error);
     return NextResponse.json(
       { error: "Failed to delete image" },
       { status: 500 }
