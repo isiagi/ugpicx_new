0e0f7afce62f04ff1051e4e32d5c52cb82623758
isiagi <isiagigeofrey0@gmail.com>
2025-06-29 05:11:34 +0300

    improve seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,13 +9,14 @@ import { Providers } from "./provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ugpicx - Beautiful Free Images & Pictures from Uganda",
+  title: "ugpicx | Beautiful Free Photos & Images from Uganda",
   description:
-    "Discover over 3 million+ high quality stock images, photos and vectors showcasing the beauty of Uganda and beyond, shared by our talented local and international community.",
+    "ugpicx offers beautiful free photos, stock images, and vectors showcasing the beauty of Uganda and beyond, shared by our talented local and international community.",
   viewport: "width=device-width, initial-scale=1",
 };
 
@@ -26,6 +27,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              name: "ugpicx",
+              url: "https://www.ugpicx.com",
+              logo: "https://www.ugpicx.com/ug.png",
+              description:
+                "ugpicx offers beautiful free photos and images showcasing the beauty of Uganda and beyond.",
+            }),
+          }}
+        />
+      </Head>
       <body className={inter.className}>
         <GoogleAnalytics />
         <ThemeProvider
